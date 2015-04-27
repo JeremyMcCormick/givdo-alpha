6464c944dc1f7b3d37c6879a68c37f8868732034
Jeremy McCormick <jermccormick@gmail.com>
2015-04-27 00:28:10 -0700

    add basic validation to models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,11 @@
 class Organization < ActiveRecord::Base   
+    
+  validates :name, :presence => true, :uniqueness => true, :allow_blank => false
+  validates :address1, :zip, :city, :email, :tax_id, :site_url, :presence => true, :allow_blank => false
+
+  validates :zip, zip_code: true     
+  validates :email, email: true
+  validates :site_url, url: true  
+  validates :donation_url, url: true
+   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
+  
   belongs_to :organization
+
+  validates :title, :message, :video_url, :organization_id, :presence => true
+  
+  validates :video_url, url: true
 end
