bc0d333406e43275ed82c21f4c3adce92100959c
Jeremy McCormick <jermccormick@gmail.com>
2015-04-27 00:25:23 -0700

    add app/validators to autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module GivdoAlpha
     #config.active_record.raise_in_transactional_callbacks = true
         
     #Koala.config.api_version = 'v2.0'
+    
+    # add custom validators path
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
   end
 end
