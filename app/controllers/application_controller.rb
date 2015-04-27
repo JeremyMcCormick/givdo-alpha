7c4eba78326daa57ce32e2d9b8722e6e7dfb911d
Jeremy McCormick <jermccormick@gmail.com>
2015-04-27 00:25:58 -0700

    rescue if current_user setup fails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
   
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue
+      @current_user = nil
+    end  
   end
   helper_method :current_user
 end
