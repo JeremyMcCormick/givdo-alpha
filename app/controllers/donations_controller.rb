573ffdb47a0ddddfc156af72e35a42d94dd748bf
Jeremy McCormick <jermccormick@gmail.com>
2015-05-05 00:41:52 -0700

    add working show to donations controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,23 +1,24 @@
 class DonationsController < ApplicationController
   
   def create
-    Rails.logger.debug params.inspect
     donation = Donation.create(donation_params)
-    Rails.logger.debug @donation            
     values = {
       business: Paypal::BUSINESS,
       cmd: "_xclick",
       upload: 1,
-      return: Paypal::APP_HOST,
-      invoice: donation[:id],
+      return: Paypal::APP_HOST + "/donations/" + donation.id.to_s,
+      invoice: donation.id,
       amount: donation.amount,
       item_name: donation.organization,
       item_number: donation.organization['id'],
       quantity: '1' 
     }
     url = Paypal::PAYPAL_HOST + "/cgi-bin/webscr?" + values.to_query
-    Rails.logger.debug "redirecting to paypal URL: " + url
     redirect_to(url)
+  end
+  
+  def show
+    @donation = Donation.find_by_id(params[:id])
   end
     
   private
